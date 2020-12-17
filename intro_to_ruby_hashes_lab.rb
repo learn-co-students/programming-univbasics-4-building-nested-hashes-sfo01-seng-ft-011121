ca8210f0a0f86a10d8087b4c118e85230db20078
Erik Huang <erikhuangdrums@gmail.com>
2020-12-17 00:13:11 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,44 +1,38 @@
 def base_hash
   { railroads: {} }
-
 end
 
 def monopoly_with_second_tier
-  pointless = base_hash
-  pointless[:railroads] = 
-    {
-      pieces: 4,
-      rent_in_dollars: {},
-      names: {}
-    }
-  pointless
+  hash = base_hash
+  hash[:railroads][:pieces] = 4
+  hash[:railroads][:rent_in_dollars] = {}
+  hash[:railroads][:names] = {}
+  hash
 end
 
 def monopoly_with_third_tier
-  tedious_methods = monopoly_with_second_tier
-  tedious_methods[:railroads][:rent_in_dollars] = {
+  hash = monopoly_with_second_tier
+  hash[:railroads][:rent_in_dollars] = {
     one_piece_owned: 25,
     two_pieces_owned: 50,
     three_pieces_owned: 100,
     four_pieces_owned: 200
   }
-  tedious_methods[:railroads][:names] = {
+  hash[:railroads][:names] = {
     reading_railroad: {},
     pennsylvania_railroad: {},
     b_and_o_railroad: {},
     shortline_railroad: {}
   }
-  tedious_methods
+  hash
 end
 
 def monopoly_with_fourth_tier
-  really_really_tedious = monopoly_with_third_tier
-  really_really_tedious[:railroads][:names] = {
-    reading_railroad: { mortgage_value: 100 },
-    pennsylvania_railroad: { mortgage_value: 200 },
-    b_and_o_railroad: { mortgage_value: 400 },
-    shortline_railroad: { mortgage_value: 800 }
-  }
-  really_really_tedious
-
+  hash = monopoly_with_third_tier
+  hash[:railroads][:names][:reading_railroad][:mortgage_value] = 100
+  hash[:railroads][:names][:pennsylvania_railroad][:mortgage_value] = 200
+  hash[:railroads][:names][:b_and_o_railroad][:mortgage_value] = 400
+  hash[:railroads][:names][:shortline_railroad][:mortgage_value] = 800
+  hash
 end
+
